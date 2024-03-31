3c34c2199721c1e85243ecd8546b394091ecd4f9
tortuguita1221 <111871409+tortuguita1221@users.noreply.github.com>
2024-03-31 12:38:51 -0300

    atualizaçao

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AvaJoaogabriel',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
